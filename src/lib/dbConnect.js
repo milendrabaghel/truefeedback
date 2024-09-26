8166c0182c8ad25b2b551a06c5bcf8d58f3e4993
milendrabaghel <2320403116@gmail.com>
2024-09-27 00:09:07 +0530

    connected database

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async function dbConnect() {
+    if (connection.isConnected) {
+        console.log('already connected to database');
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+        connection.isConnected = db.connections[0].readyState;
+        console.log('connected to database successfully');
+    } catch (error) {
+        console.log('database connection failed', error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // adjust according to your file structure
+      "./node_modules/@shadcn/ui/**/*.js", // add this line for ShadCN components
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
